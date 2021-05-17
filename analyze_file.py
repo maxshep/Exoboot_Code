e67aa0b3d00cbe0e9b7061c5a264fb7eb0b109e1
Max Shepherd <maxshepherd5790@gmail.com>
2021-05-17 18:02:17 -0400

    update analyze_file

--- analyze_file.py
+++ analyze_file.py
@@ -5,19 +5,31 @@ import constants
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import signal
 
 folder = 'exo_data'
-filename = "exo_data/20210323_1233testlevels_RIGHT.csv"
+filename = "20210514_1733RishZeroSetpoint_LEFT.csv"
 
-df = pd.read_csv(filename)
+df = pd.read_csv(folder + '/' + filename)
 
 
 plt.figure()
-plt.plot(df.loop_time, df.did_heel_strike)
-plt.plot(df.loop_time, df.gait_phase)
-plt.plot(df.loop_time, df.ankle_angle)
-plt.plot(df.loop_time, df.ankle_torque_from_current)
+plt.plot(df.loop_time, df.accel_x)
+plt.plot(df.loop_time, df.accel_y-1)
+plt.plot(df.loop_time, df.accel_z)
+plt.plot(df.loop_time, df.did_slip)
+
+b, a = signal.butter(N=2, Wn=0.01, btype='high')
+accel_x_filter = signal.lfilter(b, a, df.accel_x)
+accel_y_filter = signal.lfilter(b, a, df.accel_y-1)
+accel_z_filter = signal.lfilter(b, a, df.accel_z)
+
+plt.plot(df.loop_time, accel_x_filter)
+plt.plot(df.loop_time, accel_y_filter)
+plt.plot(df.loop_time, accel_z_filter)
+
 plt.show()
+
 
 # plt.figure()
 # plt.plot(df.loop_time, df.commanded_current)
--- perturbation_detectors.py
+++ perturbation_detectors.py
@@ -7,37 +7,43 @@ import custom_filters
 class SlipDetectorAP():
     def __init__(self,
                  data_container: Type[exoboot.Exo.DataContainer],
-                 acc_threshold_x: float = 0.5,
+                 acc_threshold_x: float = 0.2,
                  time_out: float = 5,
-                 max_acc_y: float = 0.2,
-                 max_acc_z: float = 0.2):
+                 max_acc_y: float = 0.1,
+                 max_acc_z: float = 0.1,
+                 do_filter_accels=True):
+        '''Last working with 0.5, 0.2, 0.2, no filter.'''
         self.data_container = data_container
         self.acc_threshold_x = acc_threshold_x
         self.max_acc_y = max_acc_y
         self.max_acc_z = max_acc_z
         self.timer = util.DelayTimer(time_out, true_until=True)
+        self.do_filter_accels = do_filter_accels
         self.accel_x_filter = custom_filters.Butterworth(
-            N=2, Wn=[0.01, 0.30], btype='bandpass')
+            N=2, Wn=0.01, btype='high')
         self.accel_y_filter = custom_filters.Butterworth(
-            N=2, Wn=[0.01, 0.30], btype='bandpass')
+            N=2, Wn=0.01, btype='high')
         self.accel_z_filter = custom_filters.Butterworth(
-            N=2, Wn=[0.01, 0.30], btype='bandpass')
+            N=2, Wn=0.01, btype='high')
 
     def detect(self):
-        accel_x_filt = self.accel_x_filter.filter(self.data_container.accel_x)
-        accel_y_filt = self.accel_y_filter.filter(self.data_container.accel_y)
-        accel_z_filt = self.accel_z_filter.filter(self.data_container.accel_z)
-        self.data_container.gen_var1 = accel_x_filt
-        self.data_container.gen_var2 = accel_y_filt
-        self.data_container.gen_var3 = accel_z_filt
+        accel_x = self.data_container.accel_x
+        accel_y = self.data_container.accel_y-1  # Remove effect of gravity
+        accel_z = self.data_container.accel_z
+        if self.do_filter_accels:
+            accel_x = self.accel_x_filter.filter(accel_x)
+            accel_y = self.accel_y_filter.filter(accel_y)
+            accel_z = self.accel_z_filter.filter(accel_z)
+            self.data_container.gen_var1 = accel_x
+            self.data_container.gen_var2 = accel_y
+            self.data_container.gen_var3 = accel_z
 
-        if (-1*self.data_container.accel_x > self.acc_threshold_x and
-            abs(self.data_container.accel_y-1) < + self.max_acc_y and
-                abs(self.data_container.accel_z) < self.max_acc_z and
+        if (accel_x < -1*self.acc_threshold_x and
+            abs(accel_y) < + self.max_acc_y and
+                abs(accel_z) < self.max_acc_z and
                 not self.timer.check()):
             self.timer.start()
             self.data_container.did_slip = True
-            # print('slip detected!')
         else:
             self.data_container.did_slip = False
 
