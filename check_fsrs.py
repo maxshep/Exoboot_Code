5cec5e28862fdc9a835fe46ed779ce491e591ff1
Max Shepherd <maxshepherd5790@gmail.com>
2021-05-24 16:10:43 -0400

    fsrsrsrs

--- check_fsrs.py
+++ check_fsrs.py
@@ -8,12 +8,12 @@ right_heel_FSR = Button(constants.RIGHT_HEEL_FSR_PIN)
 right_toe_FSR = Button(constants.RIGHT_TOE_FSR_PIN)
 
 while True:
-    if left_heel_FSR.is_pressed:
-        print("Left Heel is pressed")
-    if left_toe_FSR.is_pressed:
-        print("Left Toe is pressed")
-    # if right_heel_FSR.is_pressed:
-    #     print("Right Heel is pressed")
-    # if right_toe_FSR.is_pressed:
-    #     print("Right Toe is pressed")
+    # if left_heel_FSR.is_pressed:
+    #     print("Left Heel is pressed")
+    # if left_toe_FSR.is_pressed:
+    #     print("Left Toe is pressed")
+    if right_heel_FSR.is_pressed:
+        print("Right Heel is pressed")
+    if right_toe_FSR.is_pressed:
+        print("Right Toe is pressed")
     time.sleep(0.25)
