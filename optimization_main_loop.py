199615375ce72fd89d8e43eab64b5fadcbf44484
Ung Hee Lee <unghee@umich.edu>
2022-02-09 12:58:16 -0500

    Update and rename test_script_for_running_modified_main_loop.py to optimization_main_loop.py

---
+++ optimization_main_loop.py
@@ -0,0 +1,6 @@
+import sys
+pardir = os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))) + '/preference_learning_dephy_exo'
+print(pardir)
+sys.path.append(pardir)
+import main_loop
--- test_script_for_running_modified_main_loop.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append("/home/pi/Github_repo/preference_learning_dephy_exo")
-
-import main_loop
