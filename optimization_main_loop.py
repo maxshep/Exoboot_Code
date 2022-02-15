fad97de980d437b659ca7fe2c777ff7392c82361
unghee <unghee@umich.edu>
2022-02-15 14:46:59 -0500

    path changed

--- optimization_main_loop.py
+++ optimization_main_loop.py
@@ -1,7 +1,7 @@
 import sys
 import os
 pardir = os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__))) + '/preference_learning_dephy_exo'
+    os.path.abspath(__file__))) + '/preference_learning_dephy_exo/online_optimization'
 print(pardir)
 sys.path.append(pardir)
 import main_loop
